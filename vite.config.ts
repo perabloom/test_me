b547e3d5b7f87802ca48380446cc1b171025f64a
VJ <vishaldood@gmail.com>
2025-01-04 16:39:59 -0500

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',     // Binds to all network interfaces, making it externally accessible
+    port: process.env.PORT || 3000,   // Dynamically use the PORT environment variable, or default to 3000
+  },
   plugins: [react(), TanStackRouterVite()],
 })
